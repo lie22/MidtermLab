68b97137caab1508457a6735388aeaf2817e0a26
lie22 <charlie.jovita@gmail.com>
2025-11-16 00:05:24 +0800

    commit

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import (HomePageView, AboutPageView, BlogListView
+, BlogListView, BlogCreateView, BlogUpdateView, BlogDeleteView,)
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
-]
+    path('blog/', BlogListView.as_view(), name='blog'),
+    path('blog/<int:pk', BlogListView.as_view(), name='blog_detail'),
+    path('blog/create', BlogCreateView.as_view(), name='blog_create'),
+    path('blog/<int:pk>/delete', BlogCreateView.as_view(), name='blog_delete'),
--- app/views.py
+++ app/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, BlogDetailView
+from django.views.edit import CreateView, UpdateView
+from .models import Post
+from django.url import reverse_lazy
+
 
 class HomePageView(TemplateView):
     template_name = 'app/home.html'
 
 class AboutPageView(TemplateView):
     template_name = 'app/about.html'
+
+class BlogListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'app/blog_list.html'
+
+class BlogDetailView(BlogDetailView)
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'app/blog_detail.html'
+
+class BlogCreateView(CreateView)
+    model = Post
+    fields = ['title', 'author', 'body']
+    template_name = 'app/blog_create.html'
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'author', 'body']
+    template_name = 'app/blog_update.html'
+
+class BlogDeleteView(UpdateView):
+        model = Post
+        template_name = 'app/blog_delete.html'
+        success_url = reverse_lazy'home'
